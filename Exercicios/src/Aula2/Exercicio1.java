a6ce7bfb2de1b6c11023701c121362a2a13213db
Gabriel Munhoz <33963432+GabrielMunhoz@users.noreply.github.com>
2018-11-12 19:26:57 -0200

    Exercicios

---
+++ Exercicios/src/Aula2/Exercicio1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package Aula2;
+
+/**
+ * @author 631720069
+ *
+ */
+public class Exercicio1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	
+}
